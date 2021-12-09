6a1cf838840f2ca535e69bdf96daf800a32c2221
Loknath Dhar <loknath-dhar@pm.me>
2021-12-09 16:38:23 +0600

    planning

--- contact_management.cpp
+++ contact_management.cpp
@@ -2,36 +2,43 @@
 #include <fstream>	// for file management.
 #include <string.h>
 
+// the contact book class
 class contact
 {
 
 }
 
+// to save contact on the phonebook
 void save_contact()
 {
 
 }
 
+// to display contact on the command line
 void display_contact()
 {
 
 }
 
+// to delete preferred contact
 void delete_contact()
 {
 
 }
 
+// to edit and update the existing contact
 void edit_contact()
 {
 
 }
 
+// to show the list of contacts
 void show_all_contacts()
 {
 
 }
 
+// the main program
 int main()
 {
 
