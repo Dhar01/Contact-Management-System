99f46e68dfd3d57cbdbae646d3baff8faf1dbee6
Loknath Dhar <loknath-dhar@pm.me>
2021-12-09 16:35:53 +0600

    planning

--- contact_management.cpp
+++ contact_management.cpp
@@ -2,3 +2,38 @@
 #include <fstream>	// for file management.
 #include <string.h>
 
+class contact
+{
+
+}
+
+void save_contact()
+{
+
+}
+
+void display_contact()
+{
+
+}
+
+void delete_contact()
+{
+
+}
+
+void edit_contact()
+{
+
+}
+
+void show_all_contacts()
+{
+
+}
+
+int main()
+{
+
+    return 0;
+}
